780ff3100f48d46dc64b11661c6760eadabb6aa9
Thiaguss <tsantos7098@gmail.com>
2024-07-11 00:50:56 -0300

    feat: adicionando um metodo no repositorio

--- src/main/java/com/nlw/planner/participant/ParticipantRepository.java
+++ src/main/java/com/nlw/planner/participant/ParticipantRepository.java
@@ -2,7 +2,9 @@ package com.nlw.planner.participant;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ParticipantRepository extends JpaRepository<Participants, UUID> {
+    List<Participants> findByTripId(UUID triId);
 }
